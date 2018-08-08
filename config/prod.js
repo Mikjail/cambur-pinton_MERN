d531f9891d672bb0d9be28edd4fdf23d1495fae5
Mikjail Salazar <mikjailsalazar@Mikjails-MacBook-Pro.local>
2018-08-08 07:49:04 +0400

    edit var context

--- config/prod.js
+++ config/prod.js
@@ -3,7 +3,6 @@ module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret : process.env.GOOGLE_CLIENT_SECRET,
     mongoDB_URI : process.env.MONGODB_URI,
-    secret: 'mika123',
     cookieKey: process.env.COOKIE_KEY,
     mercadoPago_client_id:process.env.MERCADOPAGO_CLIENT_ID,
     mercadoPago_client_secret:process.env.MERCADOPAGO_CLIENT_SECRET,
