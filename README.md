8c00b87cb6e537d1e01ca5a2b363d034c6ce4c10
MikjailS <mikjailsalazar@gmail.com>
2023-08-18 08:39:11 +0200

    Fixing showcase web

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
-
 # CAMBUR PINTON (MERN RELEASE)
 
 ## DEMO
+
 https://guarded-savannah-73486.herokuapp.com/
 
 ## Technologies:
 
 1. Front End: React with Redux.
-2. Back End: Nodejs (9.6.1), Express (4.16.3), Mongoose (5.1.3) 
+2. Back End: Nodejs (9.6.1), Express (4.16.3), Mongoose (5.1.3)
 
 ## Folder Structure
 
@@ -26,3 +26,24 @@ server/
   package.json
 ```
 
+## Getting started
+
+1. Install the dependencies from the backend and the client side
+
+```
+npm install
+```
+
+2. Set your .env variables needed to run the app
+
+```
+
+```
+
+2. Run the app!
+
+```
+npm run dev
+```
+
+### Happy Coding!👋🏼
--- index.js
+++ index.js
@@ -1,30 +1,31 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cookieSession = require('cookie-session');
-const passport = require('passport');
-const config = require('./config/keys');
-const bodyParser = require('body-parser');
-var flash = require('connect-flash');
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const config = require("./config/keys");
+const bodyParser = require("body-parser");
+var flash = require("connect-flash");
 
-require('./models/User');
-require('./models/Product');
-require('./models/Property');
-require('./services/passport');
+require("./models/User");
+require("./models/Product");
+require("./models/Property");
+require("./services/passport");
 
-mongoose.connect(config.mongoDB_URI, (err, res) => {
-    if (err) {
-    console.log ('ERROR connecting to: ' + config.mongoDB_URI + '. ' + err);
-    } else {
-    console.log ('Succeeded connected to: ' + config.mongoDB_URI);
-    }
+mongoose
+  .connect(config.mongoDB_URI)
+  .then(() => {
+    console.log("database connected!");
+  })
+  .catch(() => {
+    console.log("Error happened and couldnt connect");
   });
 
 const app = express();
 app.use(
-    cookieSession({
-        maxAge: 30 * 24 * 60 * 1000,
-        keys: [config.cookieKey] 
-    })
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 1000,
+    keys: [config.cookieKey],
+  })
 );
 
 app.use(passport.initialize());
@@ -32,21 +33,20 @@ app.use(passport.session());
 app.use(bodyParser.json());
 app.use(flash());
 
-require('./routes/authRoutes')(app);
-require('./routes/userRoutes')(app);
-require('./routes/orderRoutes')(app);
+require("./routes/authRoutes")(app);
+require("./routes/userRoutes")(app);
+require("./routes/orderRoutes")(app);
 
-if(process.env.NODE_ENV === 'production'){
-    // 1. Express will serve up prod assets like main.css and main.js
-    app.use(express.static('client/build'));
-   
-    // 2. Express wirll serve up index.html if it doesn't recognize the file 
-    const path = require('path');
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    })
+if (process.env.NODE_ENV === "production") {
+  // 1. Express will serve up prod assets like main.css and main.js
+  app.use(express.static("client/build"));
+
+  // 2. Express wirll serve up index.html if it doesn't recognize the file
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
